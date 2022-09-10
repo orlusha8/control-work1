ae9f2abb0e7c088b98cb13e462418143a45b5e79
orlusha8 <108234027+orlusha8@users.noreply.github.com>
2022-09-10 15:22:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача:
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+
+string[] array = new string[] { "Hello", "2", "world", ":-)", "fox" };
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Formation(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+        }
+        else arr[i] = string.Empty;
+    }
+}
+
+PrintArray(array);
+Formation(array);
+Console.Write("Итог: ");
+PrintArray(array);
